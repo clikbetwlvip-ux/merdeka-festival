ef3e6376a7aced6ed3644a0073cb4ac0f8f710b3
clikbetwlvip-ux <clikbetwlvip@gmail.com>
2026-07-26 01:56:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// =========================
+// CLICKBET88 ARENA LOMBA
+// =========================
 
+const loginBtn = document.getElementById("loginBtn");
+const memberID = document.getElementById("memberID");
+
+loginBtn.addEventListener("click", function () {
+
+    if (memberID.value.trim() === "") {
+
+        alert("Silakan masukkan ID Member!");
+
+        memberID.focus();
+
+        return;
+
+    }
+
+    loginBtn.innerHTML = "MEMASUKI ARENA...";
+
+    loginBtn.disabled = true;
+
+    setTimeout(function () {
+
+        window.location.href = "game.html";
+
+    }, 1200);
+
+});
