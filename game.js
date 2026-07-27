6f8ff52d9e1726869aaa9cf0265461ab4cc1b475
clikbetwlvip-ux <clikbetwlvip@gmail.com>
2026-07-28 01:55:50 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
+"use strict";
 
+/*
+    File ini nanti digunakan untuk:
+
+    - Logika mini-game
+    - Sistem skor
+    - Sistem poin
+    - Timer permainan
+    - Hasil menang atau kalah
+    - Efek permainan
+
+    Untuk tahap pertama, file ini belum memiliki
+    permainan aktif.
+*/
+
+const GameManager = {
+    currentGame: null,
+
+    start(gameId) {
+        this.currentGame = gameId;
+
+        console.log(`Game dimulai: ${gameId}`);
+    },
+
+    stop() {
+        console.log("Game dihentikan.");
+
+        this.currentGame = null;
+    }
+};
