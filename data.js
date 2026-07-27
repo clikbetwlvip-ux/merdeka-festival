797ea3961b0a442f91d23908e4332f2bee7f627b
clikbetwlvip-ux <clikbetwlvip@gmail.com>
2026-07-28 02:11:20 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,33 @@
+"use strict";
 
+window.APP_DATA = {
+    brand: {
+        name: "CLICKBET88",
+        logo: "https://clickbet88sedekahrejeki05.xyz/storage/webpersonalize/d54f5304-03b3-49f7-b283-e6fe0c595ca7.gif"
+    },
+
+    event: {
+        name: "Fantasy Portal",
+        version: "1.0.0"
+    },
+
+    member: {
+        username: "Guest Member",
+        level: 1,
+        points: 1000,
+        tickets: 3
+    },
+
+    settings: {
+        soundEnabled: true,
+        loadingDuration: 3400
+    },
+
+    loadingMessages: [
+        "Menyiapkan dunia...",
+        "Memanggil para hero...",
+        "Membuka portal anime...",
+        "Menyiapkan efek visual...",
+        "Portal siap dibuka..."
+    ]
+};
